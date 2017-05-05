b246875d3a8f8523364ab1967b6232870f4cd194
Max Baranov <elcaribe1996@gmail.com>
2017-05-05 20:39:37 +0300

    update params in users controller

--- app/controllers/api/v1/account/users_controller.rb
+++ app/controllers/api/v1/account/users_controller.rb
@@ -1,28 +1,39 @@
 class Api::V1::Account::UsersController < Api::V1::Account::BaseController
   def create
-    if sign_up_params.blank?
+    if user_params(sign_up_attributes).blank?
       render_bad_request I18n.t('general.blank_params')
     else
-      result = Session.RegistrationService.new(sign_up_params).perform
+      result = Session.RegistrationService.new(user_params(sign_up_attributes)).perform
       result.success? ? set_token(result.data) : render_unprocessable_entity(result.error)
     end
   end
 
   def update
-    @current_user.update(update_params)
+    @current_user.update(user_params(update_attributes))
+    if game?
+      #TODO
+    end
   end
 
   private
 
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+  def user_params(permitted_attributes)
+    params.require(:user).permit(permitted_attributes)
   end
 
-  def sign_up_params
-    user_params
+  def sign_up_attributes
+    [:email, :password, :password_confirmation].append(update_attributes).append(game_attributes)
   end
 
-  def update_params
-    user_params
+  def update_attributes
+    [:first_name, :last_name, :year_of_birth]
+  end
+
+  def game_attributes
+    [games: [:game_id]]
+  end
+
+  def game?
+    params[:user][:games].present?
   end
 end
